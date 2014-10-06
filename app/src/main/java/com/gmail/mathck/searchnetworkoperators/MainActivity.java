3627cf51f712b3575ba348f9c2c1c63c9a057342
Mateusz Czernecki <mathck@gmail.com>
2014-10-06 20:23:59 +0200

    exception handling

--- app/src/main/java/com/gmail/mathck/searchnetworkoperators/MainActivity.java
+++ app/src/main/java/com/gmail/mathck/searchnetworkoperators/MainActivity.java
@@ -1,9 +1,11 @@
 package com.gmail.mathck.searchnetworkoperators;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -13,7 +15,22 @@ public class MainActivity extends Activity {
         //setContentView(R.layout.activity_main);
 
         Intent intent = new Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS);
-        startActivity(intent);
+
+        try {
+            startActivity(intent);
+        }
+        catch (Exception e1) {
+            intent.setClassName("com.android.phone", "com.android.phone.NetworkSetting");
+            startActivity(intent);
+
+            try {
+                ComponentName cName = new ComponentName("com.android.phone", "com.android.phone.NetworkSetting");
+                intent.setComponent(cName);
+            }
+            catch (Exception e2) {
+            }
+        }
+
         finish();
     }
 }
